7ded56b8f525faf7060e6a69e8fb75bc78db72ea
18937872157 <1192608424@qq.com>
2016-10-05 16:49:46 +0800

    Signed-off-by: 18937872157 <1192608424@qq.com>

---
+++ LiWuShuo/app/src/main/java/qf/com/liwushuo/MainActivity.java
@@ -0,0 +1,13 @@
+package qf.com.liwushuo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
